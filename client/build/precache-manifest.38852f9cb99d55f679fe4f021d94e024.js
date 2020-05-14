918c25f4dffe8e72cdbc57cb951a61a3f821623b
Lee Lawler <lawler_lee@icloud.com>
2020-05-13 19:53:19 -0500

    heroku deploy commit

---
+++ client/build/precache-manifest.38852f9cb99d55f679fe4f021d94e024.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f714e389faf1fcdd818c",
+    "url": "/static/js/main.db1228c3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4a23e68c2d4ba246d507",
+    "url": "/static/js/2.78f9f575.chunk.js"
+  },
+  {
+    "revision": "9917e8b1fb2d6dc069df054aba8b3df3",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -7,24 +7,18 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 routes(app)
-// Define API routes here
-
-// Send every other request to the React app
-// Define any API routes before this runs
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
-
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
-
-app.listen(PORT, () => {
+// Define API routes here Send every other request to the React app Define any
+// API routes before this runs app.get("*", (req, res) => {
+// res.sendFile(path.join(__dirname, "./client/build/index.html")); }); Connect
+// to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user40:aspire123@ds151997.mlab.com:51997/heroku_5tkpqj10");
+ app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
